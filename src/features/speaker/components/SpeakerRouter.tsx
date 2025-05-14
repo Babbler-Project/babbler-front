aed27505eab1c1fbefc1f770f2ffc4f9c1c45148
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-05-14 11:22:45 +0200

    feat(speaker): add SpeakerSchedulePage with calendar

--- src/features/speaker/components/SpeakerRouter.tsx
+++ src/features/speaker/components/SpeakerRouter.tsx
@@ -4,6 +4,7 @@ import SpeakerHomeDashboard from "../dashboard/components/SpeakerHomeDashboard";
 import SubmitTalkPage from "../talks/components/SubmitTalkPage";
 import MySubmissionsPage from "../talks/components/MySubmissionsPage";
 import { NotFound } from "@/components/shared/navigation/not-found";
+import SpeakerSchedulePage from "../schedule/components/SpeakerSchedulePage";
 
 const SpeakerRouter = () => {
   return (
@@ -12,7 +13,7 @@ const SpeakerRouter = () => {
         <Route path="/" element={<SpeakerHomeDashboard />} />
         <Route path="/talks" element={<MySubmissionsPage />} />
         <Route path="/talks/submit" element={<SubmitTalkPage />} />
-        <Route path="/schedule" element={<div>schedule</div>} />
+        <Route path="/schedule" element={<SpeakerSchedulePage />} />
         <Route path="/resources" element={<div>resources</div>} />
         <Route path="/profile" element={<div>profile</div>} />
         <Route path="/settings" element={<div>settings</div>} />
---
+++ src/features/speaker/schedule/components/SpeakerSchedulePage.tsx
@@ -0,0 +1,5 @@
+import { CalendarView } from "@/features/calendar/components/CalendarView";
+
+export default function SpeakerSchedulePage() {
+  return <CalendarView className="max-h-[calc(100vh-8rem)]" />;
+}
