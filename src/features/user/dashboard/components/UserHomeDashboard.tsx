485dc265741ca4ce51d54a6e60430c708d5e06c6
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-05-16 00:25:34 +0200

    feat(user): integrate CalendarView

--- src/features/user/dashboard/components/UserHomeDashboard.tsx
+++ src/features/user/dashboard/components/UserHomeDashboard.tsx
@@ -1,11 +1,9 @@
+import { CalendarView } from "@/features/calendar/components/CalendarView";
+
 export default function UserHomeDashboard() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      <h1 className="text-2xl font-bold">User Dashboard</h1>
-      <p className="mt-4 text-gray-600">
-        Welcome to the user dashboard! Here you can manage your account and
-        settings.
-      </p>
+    <div className="space-y-4">
+      <CalendarView className="max-h-[calc(100vh-8rem)]" />
     </div>
   );
 }
